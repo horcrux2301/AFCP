ebd8c4e4fc90b4d80e806c50d53f3ccac6a8d551
horcrux2301 <horcrux2301@yahoo.com>
2018-04-06 01:57:35 +0530

    First Commit

---
+++ spec/cp-automation-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CpAutomationView from '../lib/cp-automation-view';
+
+describe('CpAutomationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
