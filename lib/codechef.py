08dcba50f56af7db509504ac6095a07c24f2e6cf
horcrux2301 <horcrux2301@yahoo.com>
2018-04-10 23:46:08 +0530

    Improved for Codeforces

--- lib/codechef.py
+++ lib/codechef.py
@@ -1,4 +1,3 @@
-import pyautogui
 import urllib.request
 from bs4 import BeautifulSoup
 import time
---
+++ lib/template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#define ll long long
+#define si scanf("%d",&n)
+#define sl scanf("%lld",&n)
+#define pi printf("%d",n)
+#define pl printf("%d",n)
+using namespace std;
+
+int main(){
+  int test;
+  cin >> test;
+  while(test--){
+
+  }
+}
